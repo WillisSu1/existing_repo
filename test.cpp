e92c4e6bfd19dca69e2a4b60bc7d4b0e00a9150e
WenYu <willis1.sou@gmail.com>
2024-02-25 06:29:37 +0800

    Add test.cpp test2.cpp

---
+++ test.cpp
@@ -0,0 +1,7 @@
+/*Program #1 -Show Hello on Terminal*/
+#include <stdio.h>
+int main(void)
+{
+    printf("Hello! I can write program now\n");
+    return 0;
+}
---
+++ test2.cpp
@@ -0,0 +1,7 @@
+/*Program #1 -Show Hello on Terminal test2*/
+#include <stdio.h>
+int main(void)
+{
+    printf("Hello! I can write program now\n");
+    return 0;
+}
